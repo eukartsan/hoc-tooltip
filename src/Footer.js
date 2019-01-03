41f712c7a205b3e35329ecafdf4b16cf03add310
eukartsan <eugenekartsan@gmail.com>
2019-01-03 18:03:28 +0200

    add flex footer

--- src/Footer.js
+++ src/Footer.js
@@ -12,12 +12,27 @@ export default class Footer extends React.Component {
     }
     render() {
         return (
-            <div className='footer'>
-                {this.state.title}
-                <Tooltip
-                    title={this.state.title}
-                    tooltip={this.state.tooltip}
-                />
+            <div>
+                <div className="flex-container flex-end">
+                    <Tooltip
+                        title={this.state.title}
+                        tooltip={this.state.tooltip}
+                    />
+                    <div className="flex-item">
+                        <h3>{this.state.title}</h3>
+                    </div>
+                    <div className="flex-item">
+                        <p>Footer</p>
+                    </div>
+                    <ul className="flex-item">
+                        <li>Блог</li>
+                        <li>Контакты</li>
+                        <li>О нас</li>
+                    </ul>
+                    <div className="flex-item">
+                        <p>Copyright © 2019</p>
+                    </div>
+                </div>
             </div>
         )
     }
